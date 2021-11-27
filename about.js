6dc727eabf31f7cd0eed8309bf1c685f242be0f6
Alaina <alaismit1@gmail.com>
2021-11-26 18:52:43 -0700

    second commit

--- about.js
+++ about.js
@@ -1,14 +1,73 @@
-console.log("hello world");
+const button = document.getElementById('Submit')
+
+
+const class0 = document.getElementsById('uno')
+const class1 = document.getElementById('dos')
+const class2 = document.getElementById('tres')
 
 
 
-function handleSubmit(evt) {
-	evt.preventDefault();
+const formSubmit = () => {
+	alert('Your form has been submitted!');
+	}
 	
-	console.log('form submit');
+	button.addEventListener('click', formSubmit);
+
+
+const getComp = () => {
+	alert('You are awesome!');
 }
+
+class0.addEventListener('mouseover', getComp);
+
+//NOTES: compliment not showing up
 
 
 let form = document.querySelector('form#contact');
 
-form.addEventListener('submit', handleSubmit);
+// const submitForm = document.querySelector('form')
+
+// submitForm.addEventListener('submit')
+
+
+
+
+
+
+
+
+// function compliment () {
+//  document.getElementById("img1").src ="https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80">
+// }
+
+// function setRemovecComp () {
+
+// }
+
+
+
+
+
+
+
+
+
+// function complimentImg {
+// 	console.log(listGroup)
+	
+// }
+// img.addEventListener('mouseover', img);
+
+
+
+
+
+
+
+
+
+// document.querySelector("img").addEventListener("mouseover", );
+
+// I need to make a function so I can call 
+
+
